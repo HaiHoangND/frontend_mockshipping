cd6ad9a007eb788a82771b1c0785fc4381ff8cea
unknown <longtp@gmail.com>
2023-09-05 08:11:31 +0700

    route

--- src/components/createOrderPersonalInfoForm/CreateOrderPersonalInfoForm.jsx
+++ src/components/createOrderPersonalInfoForm/CreateOrderPersonalInfoForm.jsx
@@ -1,8 +1,21 @@
 import { Person, Phone } from "@mui/icons-material";
 import "./createOrderPersonalInfoForm.scss";
 import { MenuItem, TextField } from "@mui/material";
+import { districts } from "../../utils/shortestPath";
+import { useState } from "react";
 
-export const CreateOrderPersonalInfoForm = ({ person }) => {
+export const CreateOrderPersonalInfoForm = ({ person, onInputsChange }) => {
+  const [inputs, setInputs] = useState({});
+
+  const handleInputsChange = (e) => {
+    const newInputs = {
+      ...inputs,
+      [e.target.name]: e.target.value,
+    };
+    setInputs(newInputs);
+    onInputsChange(newInputs);
+  };
+
   return (
     <div className="createOrderPersonalInfoFormContainer">
       <div className="personalInfoFormTitle">
@@ -12,11 +25,7 @@ export const CreateOrderPersonalInfoForm = ({ person }) => {
         <span>
           Tên <b style={{ color: "red" }}>(*)</b> :
         </span>
-        <TextField
-          label="Tên"
-          placeholder="Trần Phi Long"
-          size="small"
-        />
+        <TextField label="Tên" placeholder="Trần Phi Long" size="small" />
       </div>
       <div className="personalInfoFormInputWrapper">
         <span>
@@ -26,27 +35,36 @@ export const CreateOrderPersonalInfoForm = ({ person }) => {
           label="Địa chỉ"
           placeholder="256 Đội Cấn"
           size="small"
+          name="detailedAddress"
+          onChange={handleInputsChange}
         />
       </div>
       <div className="personalInfoFormInputWrapper">
-      <span>
-         Số điện thoại <b style={{ color: "red" }}>(*)</b> :
-        </span>
+        <span></span>
         <TextField
-          label="Sđt"
-          placeholder="0123456789"
+          select
+          defaultValue="Quận"
+          helperText="Chọn quận"
           size="small"
-        />
+          name="district"
+          onChange={handleInputsChange}
+        >
+          {districts.map((option) => (
+            <MenuItem key={option.id} value={option.district}>
+              {option.district}
+            </MenuItem>
+          ))}
+        </TextField>
       </div>
       <div className="personalInfoFormInputWrapper">
-      <span>
-         Email :
+        <span>
+          Số điện thoại <b style={{ color: "red" }}>(*)</b> :
         </span>
-        <TextField
-          label="Email"
-          placeholder="example@email.com"
-          size="small"
-        />
+        <TextField label="Sđt" placeholder="0123456789" size="small" />
+      </div>
+      <div className="personalInfoFormInputWrapper">
+        <span>Email :</span>
+        <TextField label="Email" placeholder="example@email.com" size="small" />
       </div>
     </div>
   );
