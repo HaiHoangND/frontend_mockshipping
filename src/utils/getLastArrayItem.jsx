94703e4d3441ffa48b13f3258d69920e1d8ea4e5
unknown <longtp@gmail.com>
2023-09-07 15:20:15 +0700

    get in dex

--- src/utils/getLastArrayItem.jsx
+++ src/utils/getLastArrayItem.jsx
@@ -2,4 +2,6 @@ export const getArrayLastItem = (array) => {
   return array[array.length - 1];
 };
 
-// export const get 
+export const getIndexOfItem = (arr, id) => {
+  return arr.findIndex((item) => item.id === id);
+};
