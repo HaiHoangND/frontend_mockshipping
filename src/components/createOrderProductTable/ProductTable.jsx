c9a4ac543ec29924282b35b5b939b1eff6815158
unknown <longtp@gmail.com>
2023-09-13 12:19:50 +0700

    d

--- src/components/createOrderProductTable/ProductTable.jsx
+++ src/components/createOrderProductTable/ProductTable.jsx
@@ -1,21 +1,40 @@
 import { ReceiptLong } from "@mui/icons-material";
 import { Col, Row, Select, Space, Table } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { publicRequest } from "../../requestMethods";
 
 const { Option } = Select;
-export const ProductTable = ({ products }) => {
+export const ProductTable = () => {
+  const [orderProducts, setOrderProducts] = useState([]);
+  const [inStockProducts, setInStockProducts] = useState([]);
+
+  const getInStockProducts = async () => {
+    try {
+      const res = await publicRequest.get(
+        "/productShop/getByShopOwnerId?ShopOwnerId=1&pageNumber=1&pageSize=10"
+      );
+      setInStockProducts(res.data.data.content);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getInStockProducts();
+  }, []);
   const columns = [
     {
       title: "Hình ảnh",
       dataIndex: "image",
+      render: (image) => (
+        <div style={{ maxWidth: "100px" }}>
+          <img style={{ width: "100%" }} src={image} />
+        </div>
+      ),
     },
     {
       title: "Tên mặt hàng",
       dataIndex: "name",
-    },
-    {
-      title: "Số lượng",
-      dataIndex: "quantity",
     },
     {
       title: "Cân nặng",
@@ -29,18 +48,44 @@ export const ProductTable = ({ products }) => {
       title: "Mô tả sản phẩm",
       dataIndex: "description",
     },
+    {
+      title: "Số lượng",
+      dataIndex: "quantity",
+    },
   ];
+
+  const handleOrderProductChange = (values) => {
+    const filteredProducts = [];
+    for (const value of values) {
+      const filteredProduct = inStockProducts.find(
+        (product) => product.id === value
+      );
+      filteredProducts.push(filteredProduct);
+    }
+    setOrderProducts(filteredProducts);
+  };
+  console.log(orderProducts);
   return (
     <>
       <Select
         style={{ width: "100%", margin: "30px 0px" }}
         mode="multiple"
         placeholder="Chọn sản phẩm"
-      ></Select>
+        onChange={handleOrderProductChange}
+        filterOption={(inputValue, option) =>
+          option.label.toLowerCase().includes(inputValue.toLowerCase())
+        }
+      >
+        {inStockProducts.map((product) => (
+          <Option key={product.id} value={product.id} label={product.name}>
+            <Space>{product.name}</Space>
+          </Option>
+        ))}
+      </Select>
 
       <Table
         columns={columns}
-        dataSource={products}
+        dataSource={orderProducts}
         rowKey={(record) => record.id}
       />
     </>
